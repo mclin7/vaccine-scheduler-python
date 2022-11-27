84828bd9ab36b8a437b6e95f3560de70fa27f4e3
miancheng <mclin7@uw.edu>
2022-11-26 17:34:00 -0800

    part1

--- src/main/scheduler/model/Appointment.py
+++ src/main/scheduler/model/Appointment.py
@@ -7,8 +7,12 @@ import pymssql
 
 
 class Appointment:
-    def __init__(self, username, time):
-        self.username = username
+    def __init__(self, patient_name, caregiver_name, vaccines_name, time):
+        self.ID = None
+        self.patient_name = patient_name
+        self.caregiver_name = caregiver_name
+        self.vaccines_name = vaccines_name
         self.time = time
+
 
 # TBD in part 2
